315d79e6e8848b7fd31197c5084688ee1bac791b
m1stadev <adamhamdi31@gmail.com>
2022-01-20 16:01:57 -0600

    [main] Lot of changes - Change endpoints - Don't use BackgroundTasks - Require API key to be passed as header - Add error handling for KeyError - Add endpoint to set brightness - Add endpoint to set color - Add endpoint to get device names - Remove seizure endpoint - Now require device to be passed to API

--- main.py
+++ main.py
@@ -1,59 +1,77 @@
 from datetime import datetime
 from fastapi.responses import JSONResponse
-from fastapi import BackgroundTasks, FastAPI, HTTPException, Request
+from fastapi import FastAPI, Header, HTTPException, Request
 from utils.api import Govee
-from utils import errors
+from utils import errors, types
 
-import random
-import time
 
 API_KEY = ''
 
 app = FastAPI()
 api = Govee(API_KEY)
 
+@app.get('/govee/devices')
+def get_devices(): return [_['device'] for _ in api.devices]
 
-def _disable_lights():
-    devices = api.devices
-    api.disable(devices[0])
+@app.post('/govee/actions/enable')
+def enable_lights(device: types.DeviceData, api_key: str=Header(None)):
+    if api_key != API_KEY:
+        raise errors.AuthError('Invalid API key provided.')
 
-def _enable_lights():
-    devices = api.devices
-    api.enable(devices[0])
-
-def _seizure():
-    api.seizure_running = True
     devices = api.devices
 
-    api.set_brightness(devices[0], 100)
+    try:
+        device = next(_ for _ in devices if _['device'] == device.name)
+    except StopIteration:
+        raise HTTPException(status_code=404, detail=f"Device not found: '{device.name}'.")
 
-    start_time = time.time()
-    while (time.time() - start_time) < 10:
-        colors = [_ for _ in Color.__dict__ if not _.startswith('__')]
-        api.set_color(devices[0], random.choice(colors))
-
-    api.seizure_running = False
-
-@app.get('/govee/enable')
-def enable_lights(task: BackgroundTasks):
-    task.add_task(_enable_lights)
-
+    api.enable(device)
     return {'status': 'ok'}
 
-@app.get('/govee/disable')
-def disable_lights(task: BackgroundTasks):
-    task.add_task(_disable_lights)
+@app.post('/govee/actions/disable')
+def disable_lights(device: types.DeviceData, api_key: str=Header(None)):
+    if api_key != API_KEY:
+        raise errors.AuthError('Invalid API key provided.')
 
+    devices = api.devices
+
+    try:
+        device = next(_ for _ in devices if _['device'] == device.name)
+    except StopIteration:
+        raise HTTPException(status_code=404, detail=f"Device not found: '{device.name}'.")
+
+    api.disable(device)
     return {'status': 'ok'}
 
-@app.get('/govee/seizure')
-def seizure(task: BackgroundTasks):
-    if not api.seizure_running:
-        task.add_task(_seizure)
+@app.post('/govee/set/brightness')
+def set_brightness(device: types.DeviceData, brightness: types.BrightnessData, api_key: str=Header(None)):
+    if api_key != API_KEY:
+        raise errors.AuthError('Invalid API key provided.')
 
-        return {'status': 'ok'}
-    else:
-        raise HTTPException(status_code=425, detail='Seizure lights already running.')
+    devices = api.devices
+
+    try:
+        device = next(_ for _ in devices if _['device'] == device.name)
+    except StopIteration:
+        raise HTTPException(status_code=404, detail=f"Device not found: '{device.name}'.")
+
+    api.set_brightness(device, brightness.level)
+    return {'status': 'ok'}
+
+@app.post('/govee/set/color')
+def set_color(device: types.DeviceData, color: types.ColorData, api_key: str=Header(None)):
+    if api_key != API_KEY:
+        raise errors.AuthError('Invalid API key provided.')
+
+    devices = api.devices
+
+    try:
+        device = next(_ for _ in devices if _['device'] == device.name)
+    except StopIteration:
+        raise HTTPException(status_code=404, detail=f"Device not found: '{device.name}'.")
+
+    api.set_color(device, color.color)
+    return {'status': 'ok'}
 
 @app.exception_handler(errors.AuthError)
 def auth_error(request: Request, e: errors.APIError): return JSONResponse(status_code=401, content={'error': 'Invalid API key provided.'})
@@ -65,3 +83,6 @@ def devices_error(request: Request, e: errors.DevicesError): return JSONResponse
 def ratelimit_error(request: Request, e: errors.RatelimitError):
     sleep = round((e.time - datetime.now()).total_seconds(), 3)
     return JSONResponse(status_code=429, content={'error': f'Ratelimit reached, please wait {sleep}s.'})
+
+@app.exception_handler(KeyError)
+def keyerror_error(request: Request, e: KeyError): return JSONResponse(status_code=400, content={'error': str(e)})
